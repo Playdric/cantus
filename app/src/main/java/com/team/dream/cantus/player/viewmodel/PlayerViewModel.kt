9a7ca3ef3326b67976e0a828e68431f51b86c831
Cédric Nanteau <cedric.nanteau@gmail.com>
2019-06-30 02:41:51 +0200

    handle media player in PlayerViewModel

--- app/src/main/java/com/team/dream/cantus/player/viewmodel/PlayerViewModel.kt
+++ app/src/main/java/com/team/dream/cantus/player/viewmodel/PlayerViewModel.kt
@@ -1,8 +1,11 @@
 package com.team.dream.cantus.player.viewmodel
 
+import android.media.MediaPlayer
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.team.dream.cantus.R
 import com.team.dream.cantus.cross.model.DeezerAlbum
 import com.team.dream.cantus.cross.model.DeezerTrack
 import com.team.dream.cantus.cross.rx.RxBus
@@ -10,44 +13,87 @@ import com.team.dream.cantus.cross.rx.RxEvent
 import io.reactivex.disposables.Disposable
 
 class PlayerViewModel : ViewModel() {
+
     private val albumMutableLiveData = MutableLiveData<DeezerAlbum>()
     private val trackMutableLiveData = MutableLiveData<DeezerTrack>()
+    private val isPlayingMutableLiveData = MutableLiveData<Boolean>()
     val albumLiveData: LiveData<DeezerAlbum> = albumMutableLiveData
     val trackLiveData: LiveData<DeezerTrack> = trackMutableLiveData
+    val isPlayingLiveData: LiveData<Boolean> = isPlayingMutableLiveData
 
     private var disposable: Disposable
     private var currentAlbumTracks: List<DeezerTrack>? = null
+    private var mediaPlayer = MediaPlayer()
 
     init {
         disposable = RxBus.listen(RxEvent.EventTrackSelection::class.java).subscribe {
             albumMutableLiveData.value = it.album
-            trackMutableLiveData.value = it.selectedTrack
             currentAlbumTracks = it.tracks
+            updateTrack(it.selectedTrack)
         }
     }
 
-    fun getNext() {
+    private fun getNext() {
         currentAlbumTracks?.run {
             var index = this.indexOfFirst { it.id == trackLiveData.value?.id }
             if (index == this.lastIndex) {
-                trackMutableLiveData.value = this[0]
+                updateTrack(this[0])
                 return
             }
 
-            trackMutableLiveData.value = this[++index]
+            updateTrack(this[++index])
         }
     }
 
-    fun getPrevious() {
+    private fun getPrevious() {
         currentAlbumTracks?.run {
             var index = this.indexOfFirst { it.id == trackLiveData.value?.id }
             if (index == 0) {
-                trackMutableLiveData.value = this.last()
+                updateTrack(this.last())
                 return
             }
 
-            trackMutableLiveData.value = this[--index]
+            updateTrack(this[--index])
         }
+    }
+
+    private fun updateTrack(track: DeezerTrack) {
+        trackMutableLiveData.value = track
+
+        try {
+            mediaPlayer.reset()
+            mediaPlayer.setDataSource(track.preview)
+            mediaPlayer.prepare()
+            play()
+        } catch (err: Exception) {
+            err.printStackTrace()
+        }
+    }
+
+    private fun play() {
+        mediaPlayer.start()
+        isPlayingMutableLiveData.value = true
+    }
+
+    private fun pause() {
+        mediaPlayer.pause()
+        isPlayingMutableLiveData.value = false
+    }
+
+    fun onClickPlayPause() {
+        if (mediaPlayer.isPlaying) {
+            pause()
+        } else {
+            play()
+        }
+    }
+
+    fun onClickPrevious() {
+        getPrevious()
+    }
+
+    fun onClickNext() {
+        getNext()
     }
 
     fun onDestroy() {
